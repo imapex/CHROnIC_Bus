78a8ce3097a32b2ee470c72a3e68acb9fba41dcc
Josh Anderson <joshand@cisco.com>
2016-11-06 21:42:06 -0600

    updated README, requirements, add sample secrets, comment app.py

--- README.md
+++ README.md
@@ -1,6 +1,104 @@
 # CHROnIC_Bus
-Microservice to Accept and Distribute Jobs. Not designed to be an especially feature rich message bus,
-but created for a specific purpose - to asynchronously send/receive messages via HTTP REST API. This
-bus is designed to sit in a public infrastructure and be reachable from environments that sit
-behind a HTTP Proxy server, with limited outbound connectivity.
+##**Cisco Health Report & ONline Information Collector**
+This is an application designed to interact with infrastructure components, and perform an online analysis of them.
 
+*The Challenge* - It can be tedious to collect the data needed for routine health analysis. For example, the UCS Health Check requres a PowerShell script to be ran, which requires the UCS PowerTools to be installed, as well as a number of security requirements. This can be very complex for anyone who does not routinely use PowerShell.
+
+*The Solution* - Create an application that will enable interaction with UCS from the cloud. This particular application has several microservices, including:
+
+* Bus - Used as a basic HTTP-based message queue
+* Collector - On-prem component used to exchange core information between on-prem infrastructure and the Portal. Consumes messages from the queue.
+* Portal - Information Collection service and agent used to push tasks into the queue.
+* TBD
+
+Contributors - Josh Anderson, Chad Peterson, Loy Evans
+
+This repo is for the following service:
+Bus - This microservice to designed to accept and distribute jobs from a queue. Not designed to be an especially feature rich message bus, but created for a specific purpose - to asynchronously send/receive messages via HTTP REST API. This bus is designed to sit in a public infrastructure and be reachable from environments that sit behind a HTTP Proxy server, with limited outbound connectivity.
+
+This repo includes the following resources:
+**Repo Information**
+* README.md
+    * This document
+* .gitignore
+    * Standard gitignore file to prevent commiting unneeded or security risk files
+
+**CICD Build Configuration**
+* .drone.yml
+    * CICD Build instructions for Drone Server
+* drone_secrets_sample.yml
+    * template for the secrets file that will be used to encrypt credentials
+* Dockerfile
+    * Docker build file for applicaiton container
+* requirements.txt
+    * pip installation requirements
+
+**Application Files**
+* app.py
+    * This kicks off the application. This application listens and is delivered strictly as an API.
+* test.py
+    * Flask build test application. Used to validate API functionality for the build process.
+
+# Installation
+
+## Environment
+
+* [Docker](#opt1) Docker Container
+* [Python](#opt2) Native Python
+
+## Docker Installation
+
+**Get the container:**
+The latest build of this project is available as a Docker image from Docker Hub:
+
+    docker pull joshand/chronic_bus:latest
+
+**Run the application:**
+
+    docker run -d -p 5000:5000 --name Dockerfile joshand/chronic_hub:latest
+
+
+**Use the API:**
+[API Usage](#api) See below for API usage
+
+## Local Python Installation
+
+**Prerequisites:**
+The following components are required to locally run this project:
+* Python 3.5
+* git
+* pip
+* virtualenv
+
+**Get the code:**
+The latest build of this project is available on Github:
+
+    mkdir ~/chronic_bus
+    cd ~/chronic_bus
+    git clone https://github.com/joshand/CHROnIC_Bus
+
+**Set up virtual environment and PIP:**
+
+    virtualenv chronic
+    source chronic/bin/activate
+    pip install -r requirements.txt
+
+**Execute the app:**
+
+    python app.py
+
+# API Usage
+**Note: This usage assumes you are executing on localhost**
+* 127.0.0.1:5000/api/send/<channelid> POST
+    * {
+          "coldata": "your data or message here",
+          "status": "1",
+          "webhook": "https://url_for_webhook"
+      }
+* 127.0.0.1:5000/api/get/<channelid>  GET
+* 127.0.0.1:5000/api/send/<channelid> DELETE
+* 127.0.0.1:5000/api/status/<taskid>  POST
+    * {
+          "status": "2"
+      }
+* 127.0.0.1:5000/api/status/<taskid>  GET
--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
 from flask import Flask, request, Response
-from flask_restful import Resource, Api, reqparse
 from flask_dataset import Dataset
-import json, requests
+import json
+import requests
 
+# -- Application Setup
 app = Flask(__name__)
 app.config['DATASET_DATABASE_URI'] = 'sqlite:///:memory:'
-#app.config['DATASET_SCHEMA'] = 
 db = Dataset(app)
 
+
+# -- Add default route (/) for basic health check
 @app.route('/', methods=['GET'])
 def return_blank():
     return ""
 
+
+# -- Add route to GET status for a task
 @app.route('/api/status/<taskid>', methods=['GET'])
 def get_status(taskid):
     table = db['msgbus']
@@ -20,72 +24,87 @@ def get_status(taskid):
     mcount = table.count(id=taskid)
     resp = ""
     if mcount == 0:
+        # If there is not a task with the specified ID, return 404
         resp = Response("", status=404, mimetype='application/json')
     else:
+        # Loop through tasks (should only be one), add to json array
         arr_messages = []
         for message in messages:
             arr_messages.append(json.loads(json.dumps(message)))
         resp = json.dumps(arr_messages)
     return resp
 
+
+# -- Add route to POST new status for a task
 @app.route('/api/status/<taskid>', methods=['POST'])
 def update_task(taskid):
     content = request.json
-    #print(content)
     newstatus = content['status']
     resp = ""
     with app.test_request_context():
-        #data = dict(id=taskid, status=newstatus)
-        #retval = db['msgbus'].update(data, ['id'])
         message = db['msgbus'].find_one(id=taskid)
-        #print(message)
-        if message != None:
+        # If the task exists, perform an update
+        if message is not None:
             retval = UpdateStatus(message, newstatus)
-            #print(retval)
         else:
             retval = 0
         if retval == 0:
+            # If the task does not exist, or if there was a problem, return 404
             resp = Response("", status=404, mimetype='application/json')
         else:
+            # Return 200 ok
             resp = Response("", status=200, mimetype='application/json')
     return resp
 
+
+# -- Add route to DELETE all tasks for a specified collector
 @app.route('/api/send/<collectorid>', methods=['DELETE'])
 def clear_bus(collectorid):
+    # Default return 204 deleted
     resp = Response("", status=204, mimetype='application/json')
     with app.test_request_context():
         mcount = db['msgbus'].count(colid=collectorid)
+        # If there are no tasks for the specified collector, return 404
         if mcount == 0:
             resp = Response("", status=404, mimetype='application/json')
         else:
-            retval = db['msgbus'].delete(colid=collectorid)
-    #resp = Response("", status=204, mimetype='application/json')
+            db['msgbus'].delete(colid=collectorid)
     return resp
 
+
+# -- Add route to POST new task for a specified collector
 @app.route('/api/send/<collectorid>', methods=['POST'])
 def send_message(collectorid):
     content = request.json
     content['colid'] = collectorid
+    # Add new task to the queue for the specified collector
     with app.test_request_context():
         retval = db['msgbus'].insert(content)
     text = str(retval)
     return text
 
+
+# -- Add route to GET all tasks for specified collector
 @app.route('/api/get/<collectorid>', methods=['GET'])
 def get_message(collectorid):
     table = db['msgbus']
     messages = table.find(colid=collectorid)
     mcount = db['msgbus'].count(colid=collectorid)
+    # If there are no tasks for the specified collector, return 404 not found
     if mcount == 0:
         resp = Response("", status=404, mimetype='application/json')
     else:
+        # If there are tasks, loop through and dump in json array
         arr_messages = []
         for message in messages:
             arr_messages.append(json.loads(json.dumps(message)))
             UpdateStatus(message, 1)
-        resp = json.dumps(arr_messages) 
+        resp = json.dumps(arr_messages)
     return resp
 
+
+# -- Function used to update status on a task - used when manual task update is
+# POSTed or when a GET is done for all tasks
 def UpdateStatus(message, newstatus):
     messageid = message['id']
     url = ""
@@ -100,13 +119,13 @@ def UpdateStatus(message, newstatus):
 
     data = dict(id=messageid, status=newstatus)
     retval = db['msgbus'].update(data, ['id'])
-    #res = db.query('UPDATE msgbus SET status="' + str(newstatus) + '" WHERE id="' + str(messageid) + '"')
+    # if a webhook was specified, call the webhook since there was an update
     if url != "":
-        response = requests.request("POST", url, data=jsondata, headers=headers)
-
+        requests.request("POST", url, data=jsondata, headers=headers)
     return retval
 
+
+# -- Main function
 if __name__ == '__main__':
     # Run Flask
     app.run(debug=True, host='0.0.0.0', port=int("5000"))
-    # pass
